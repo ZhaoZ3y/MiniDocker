e06d04a63c086b37d601aed38dfbca57c27fa389
ZhaoZ3y <zzsy769174850@gamil.com>
2025-04-29 19:05:16 +0800

    修复停止容器失败问题2.0

--- stop.go
+++ stop.go
@@ -25,18 +25,22 @@ func stopContainer(containerName string) {
 		logrus.Errorf("PID 转换失败: %v", err)
 		return
 	}
-	logrus.Infof("容器 %s 的 PID: %d", containerName, pidInt)
+
 	// 检查 PID 是否存在
-	if _, err := os.FindProcess(pidInt); err != nil {
-		logrus.Errorf("容器 %s 的进程不存在: %v", containerName, err)
+	_, err = os.FindProcess(pidInt)
+	if err != nil {
+		logrus.Errorf("无法找到容器 %s 的进程: %v", containerName, err)
 		return
 	}
+
+	logrus.Infof("容器 %s 进程存在，准备发送信号", containerName)
 
 	// 发送 SIGTERM 信号给容器进程，优雅停止
 	if err := syscall.Kill(pidInt, syscall.SIGTERM); err != nil {
 		logrus.Errorf("停止容器 %s 失败: %v", containerName, err)
 		return
 	}
+
 	// 跟据容器名称获取容器信息
 	info, err := getContainerInfoByName(containerName)
 	if err != nil {
